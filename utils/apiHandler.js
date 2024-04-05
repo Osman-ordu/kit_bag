af37b271d124eac1d79a21ac1b101f521d812b97
Osman Ordu <osman.ordu@primetech.com>
2024-04-05 16:21:01 +0300

    Import axios

--- utils/apiHandler.js
+++ utils/apiHandler.js
@@ -186,24 +186,3 @@ axios.interceptors.response.use(
     throw err;
   }
 );
-
-export const CallApi = async ({ method, url, data }) => {
-  const token = localStorage.getItem('token');
-  const request = await axios({
-    method: method,
-    url: `${apiEnvUrl}${url}`,
-    data: data ?? {},
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((response) => {
-      toastSuccess(response.statusText && response.statusText !== '' ? response.statusText : "Connection Successful");
-      return response.data;
-    })
-    .catch((error) => {
-      toastError(error.response.data.message ? error.response.data.message : error.message && error.message !== '' ? error.message : 'Connection Unsuccessful');
-    });
-
-  return request;
-};
