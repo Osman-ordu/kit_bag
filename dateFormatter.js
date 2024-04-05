08f984c328c31dfe04fe714863e13d34ea7b9da3
Osman Ordu <orduosmann@gmail.com>
2024-04-05 15:59:26 +0300

    Create Date Formatter

---
+++ dateFormatter.js
@@ -0,0 +1,9 @@
+export const formatterDate = (time: string): string => {
+  if (time !== null) {
+    const correctDateString = new Date(time);
+    const formattedTime = correctDateString.toLocaleDateString() + ' ' + correctDateString.toLocaleTimeString();
+    return formattedTime;
+  } else {
+    return (time = '-');
+  }
+};
